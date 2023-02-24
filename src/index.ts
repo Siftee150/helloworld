0cedce648ea3db94b2c4c3351fcfe58f706ecd5c
Siftee15 <sifs.ratra5677@gmail.com>
2023-02-24 14:21:44 +0530

    Check: recheck

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Worldsss!");
   fetch("https://dummyjson.com/todos")
     .then((res) => res.json())
     .then((res) => {
